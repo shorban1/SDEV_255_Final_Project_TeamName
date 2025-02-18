5bbb59b5d267df00de3763fdb977e5205782b2e9
shorban1 <shorban1@ivytech.edu>
2025-02-18 17:27:30 -0500

    Workflow Removal Attempt

--- react-project/vite.config.js
+++ react-project/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  //base: "/SDEV_255_Final_Project_TeamName/",
+  base: "/SDEV_255_Final_Project_TeamName/",
   plugins: [react()],
 });
