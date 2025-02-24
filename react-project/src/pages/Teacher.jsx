a9c5b4ee66464a6ac22b676fbf2360883d41ad6e
shorban1 <shorban1@ivytech.edu>
2025-02-24 17:28:14 -0500

    Initial backend connection

--- react-project/src/pages/Teacher.jsx
+++ react-project/src/pages/Teacher.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-const Teacher = () => (
-  <div id="content">
-    <h1>Teacher</h1>
-  </div>
-);
+function Teacher() {
+  useEffect(() => {
+    async function fetchCourses() {
+      const response = await fetch(
+        "https://carbonated-rich-sumac.glitch.me/api/courses"
+      );
+      const courses = await response.json();
+
+      let html = "";
+      for (let course of courses) {
+        html += `<a href="#/courses?id=${course._id}">${course.title} - ${course.code}</a> - <a href="edit.html?id=${course._id}">Edit</a></li>`;
+      }
+
+      document.querySelector("#course-list").innerHTML = html;
+    }
+
+    fetchCourses();
+  });
+
+  return (
+    <div id="content">
+      <div>
+        <h1>Courses</h1>
+        <button>+ Add</button>
+      </div>
+      <div id="course-list"></div>
+    </div>
+  );
+}
 
 export default Teacher;
